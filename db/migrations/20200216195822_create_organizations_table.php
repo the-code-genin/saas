71299d6ecd6eb5027e11b482a34f1c3ba21d7882
Iyiola-am <adekunle3317@gmail.com>
2020-02-16 21:06:52 +0100

    Organizations and students migrations implemented

---
+++ db/migrations/20200216195822_create_organizations_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Cradle\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class CreateOrganizationsTable extends Migration
+{
+    public function up()
+    {
+        $this->schema->create('organizations', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name', 300)->unique();
+            $table->string('address', 300);
+            $table->text('description');
+            $table->integer('category_id')->unsigned();
+            $table->timestamps();
+
+            $table->foreign('category_id')
+                ->references('id')
+                ->on('organization_categories')
+                ->onDelete('update');
+        });
+    }
+
+    public function down()
+    {
+        $this->schema->drop('organizations');
+    }
+}
---
+++ db/migrations/20200216200228_create_organization_categories_table.php
@@ -0,0 +1,21 @@
+<?php
+
+use Cradle\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class CreateOrganizationCategoriesTable extends Migration
+{
+    public function up()
+    {
+        $this->schema->create('organization_categories', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name', 300);
+            $table->timestamps();
+        });
+    }
+
+    public function down()
+    {
+        $this->schema->drop('organization_categories');
+    }
+}
