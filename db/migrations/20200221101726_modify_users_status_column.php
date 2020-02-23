66ded658b5c0a5774810229add0e66f466895305
Iyiola-am <adekunle3317@gmail.com>
2020-02-23 23:00:14 +0100

    Updated migrations

--- db/migrations/20200221101726_modify_users_status_column.php
+++ db/migrations/20200221101726_modify_users_status_column.php
@@ -8,13 +8,13 @@ class ModifyUsersStatusColumn extends Migration
     public function up()
     {
         $this->db->connection()->getPdo()
-            ->exec("ALTER TABLE `users` MODIFY COLUMN `status` ENUM('pending','active','banned') DEFAULT 'pending' NOT NULL;");
+            ->exec("ALTER TABLE users MODIFY COLUMN status ENUM('pending','active','banned') DEFAULT 'pending' NOT NULL;");
     }
 
     public function down()
     {
         $this->db->table('users')->where('status', 'pending')->update(['status' => 'active']);
         $this->db->connection()->getPdo()
-            ->exec("ALTER TABLE `users` MODIFY COLUMN `status` ENUM('active','banned') DEFAULT 'active' NOT NULL;");
+            ->exec("ALTER TABLE users MODIFY COLUMN status ENUM('active','banned') DEFAULT 'active' NOT NULL;");
     }
 }
