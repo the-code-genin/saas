dc64b709dc0581c52310bb4ee294220b70debe7b
Iyiola-am <adekunle3317@gmail.com>
2020-02-23 20:06:42 +0100

    Migration to modify users column implemented

---
+++ db/migrations/20200221101726_modify_users_status_column.php
@@ -0,0 +1,19 @@
+<?php
+
+use Cradle\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class ModifyUsersStatusColumn extends Migration
+{
+    public function up()
+    {
+        $this->db->connection()->getPdo()
+            ->exec("ALTER TABLE `users` MODIFY COLUMN `status` ENUM('pending','active','banned') DEFAULT 'pending' NOT NULL;");
+    }
+
+    public function down()
+    {
+        $this->db->connection()->getPdo()
+            ->exec("ALTER TABLE `users` MODIFY COLUMN `status` ENUM('active','banned') DEFAULT 'active' NOT NULL;");
+    }
+}
