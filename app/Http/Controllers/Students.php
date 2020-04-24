6ff8137410681f2bcd151bbf465559eb10935ccd
Iyiola-am <adekunle3317@gmail.com>
2020-04-25 00:14:39 +0100

    Refactored students controller

--- app/Http/Controllers/Students.php
+++ app/Http/Controllers/Students.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Exceptions\AuthenticationError;
+use App\Exceptions\InvalidFormDataError;
 use App\Models\Job;
 use App\Helpers\Api;
 use App\Models\User;
@@ -13,6 +15,7 @@ use App\Models\JobApplication;
 use App\Notifications\StudentApplicationSubmitted;
 use Illuminate\Support\Facades\Notification;
 use Illuminate\Support\Facades\Validator;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class Students extends Controller
 {
@@ -36,18 +39,14 @@ class Students extends Controller
      * update profile visits for a student.
      *
      * @param \Illuminate\Http\Request $request
-     * @param int $id
+     * @param User $student
      *
      * @return void
      */
-    public function updateVisits(Request $request, int $id): array
+    public function updateVisits(Request $request, User $student): array
     {
-        $student = User::where('id', $id)
-            ->where('userable_type', Student::class)
-            ->first();
-
-        if (is_null($student)) { // If the expert was not found
-            return Api::generateErrorResponse(404, 'NotFoundError', 'The resource you requested for was not found.');
+        if ($student->userable_type != Student::class) {
+            throw new NotFoundHttpException('The resource you requested for was not found.');
         }
 
         // Add a viewed record to the database.
@@ -67,20 +66,15 @@ class Students extends Controller
      * Apply for a job.
      *
      * @param \Illuminate\Http\Request $request
-     * @param int $id
+     * @param Job $job
      *
      * @return array
      */
-    public function applyForJob(Request $request, int $id): array
+    public function applyForJob(Request $request, Job $job): array
     {
-        $job = Job::where('id', $id)->first();
-        if (is_null($job)) { // If the job was not found
-            return Api::generateErrorResponse(404, 'NotFoundError', 'The resource you requested for was not found.');
-        }
-
         // Check if the user can apply for the job.
         if (!$request->user()->can('apply', $job)) {
-            return Api::generateErrorResponse(401, 'AuthenticationError', 'You can not apply for the job.');
+            throw new AuthenticationError('You can not apply for the job.');
         }
 
         // Validate input.
@@ -91,7 +85,7 @@ class Students extends Controller
         ]);
 
         if ($validator->fails()) { // Validation fails.
-            return Api::generateErrorResponse(105, 'InvalidFormDataError', Api::getFirstValidationError($validator));
+            throw new InvalidFormDataError(Api::getFirstValidationError($validator));
         }
 
         // Apply for the job.
