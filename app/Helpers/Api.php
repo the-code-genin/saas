c4f7d551a30b06e79996d7b5356b69ac59093f76
Iyiola-am <adekunle3317@gmail.com>
2020-04-24 20:37:12 +0100

    Refactored api helper

--- app/Helpers/Api.php
+++ app/Helpers/Api.php
@@ -2,6 +2,8 @@
 
 namespace App\Helpers;
 
+use Illuminate\Http\Request;
+use Illuminate\Support\Collection;
 use Illuminate\Validation\Validator;
 
 class Api
@@ -41,5 +43,44 @@ class Api
         $fields = $errors->keys();
 
         return $errors->first($fields[0]);
+    }
+
+    /**
+     * Generate API payload.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @param Collection $results
+     *
+     * @return array
+     */
+    public static function getPayload(Request $request, Collection $results): array
+    {
+        $payload = [];
+
+        if (!empty($request->get('page')) || !empty($request->get('perPage'))) { // If pagination is to be applied.
+            $page = $request->get('page', 1);
+            $perPage = $request->get('perPage', 10);
+
+            /** @var Paginator */
+            $results = $results->paginate($perPage, ['*'], 'results', $page);
+
+            $payload = [
+                'total' => $results->total(),
+                'per_page' => $results->perPage(),
+                'current_page' => $results->currentPage(),
+                'prev_page' => ($results->currentPage() > 1) ? $results->lastPage() : null,
+                'next_page' => $results->hasMorePages() ? ($results->currentPage() + 1) : null,
+                'from' => $results->firstItem(),
+                'to' => $results->lastItem(),
+                'data' => $results->items(),
+            ];
+        } else { // If all are to be gotten at once.
+            $payload = [
+                'data' => $results->get(),
+                'total' => $results->count(),
+            ];
+        }
+
+        return $payload;
     }
 }
