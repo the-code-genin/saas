3705950ee81aa6dc608ff941c7dc7c3f673615fe
Iyiola-am <adekunle3317@gmail.com>
2020-04-25 16:28:24 +0100

    Updated emails

--- resources/views/emails/welcome.blade.php
+++ resources/views/emails/welcome.blade.php
@@ -2,7 +2,11 @@
 
 # Hello, {{ $user->user_type == 'student' ? $user->userable->full_name : $user->userable->name }}!
 
-Welcome to the Students as a Service platfrom! We are so glad that you have decided to joined our community.
+Welcome to the Students as a Service platfrom!
+
+We are so glad that you have decided to join our community.
+
+There is just one extra thing to do.
 
 Please follow the link below to verify your account.
 
