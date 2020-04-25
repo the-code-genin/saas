a7c538462624e6694bf0cc9407d53f3965d58134
Iyiola-am <adekunle3317@gmail.com>
2020-04-25 01:22:49 +0100

    Modified student availability column

---
+++ database/migrations/2020_04_25_011411_add_new_user_availability.php
@@ -0,0 +1,65 @@
+<?php
+
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddNewUserAvailability extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('students', function(Blueprint $table) {
+            $table->text('_temp_column')->nullable();
+        });
+
+        DB::update('UPDATE students SET _temp_column = availability', []);
+
+        Schema::table('students', function(Blueprint $table) {
+            $table->dropColumn('availability');
+        });
+
+        Schema::table('students', function(Blueprint $table) {
+            $table->enum('availability', ['freelance', 'full_time', 'part_time', 'week_days', 'week_ends']);
+        });
+
+        DB::update('UPDATE students SET availability = _temp_column', []);
+
+        Schema::table('students', function(Blueprint $table) {
+            $table->dropColumn('_temp_column');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('students', function(Blueprint $table) {
+            $table->text('_temp_column')->nullable();
+        });
+
+        DB::update('UPDATE students SET _temp_column = availability', []);
+
+        Schema::table('students', function(Blueprint $table) {
+            $table->dropColumn('availability');
+        });
+
+        Schema::table('students', function(Blueprint $table) {
+            $table->enum('availability', ['freelance', 'part_time']);
+        });
+
+        DB::update('UPDATE students SET availability = _temp_column', []);
+
+        Schema::table('students', function(Blueprint $table) {
+            $table->dropColumn('_temp_column');
+        });
+    }
+}
