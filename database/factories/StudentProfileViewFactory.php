4f8dc7c5156bb76b679df243c2f03afe6f885340
Iyiola-am <adekunle3317@gmail.com>
2020-04-25 14:09:15 +0100

    Added student profile view factory

---
+++ database/factories/StudentProfileViewFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Models\StudentProfileView;
+use Faker\Generator as Faker;
+
+$factory->define(StudentProfileView::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
